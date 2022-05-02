6f0d5666c27834795c07f5b2836b4a2a945f5bd5
Laruno <Laman_Art@outlook.com>
2022-05-03 00:09:02 +0100

    Stage 2: Degradation

---
+++ Preprocessin-Degradation.py
@@ -0,0 +1,27 @@
+import cv2
+from cv2 import GaussianBlur
+from matplotlib.pyplot import yticks
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+Samp1 = cv2.imread("Greyscaled/SampleOne.png")
+Samp2 = cv2.imread("Greyscaled/SampleTwo.png")
+#cv2.imshow("Sample One Original", Samp1)
+#cv2.imshow("Sample Two Original", Samp2)
+gaussianx = GaussianBlur(Samp1, (25,25), 0)
+gaussiany = GaussianBlur(Samp2, (25,25), 0)
+
+titles = ["Original 1","Original 2", "Gaussan Output 1","Gaussan Output 2"]
+images = [Samp1,Samp2,gaussianx,gaussiany]
+for i in range(4):
+    plt.subplot(2,2,i+1)
+    plt.imshow(images[i])
+    plt.title(titles[i])
+    plt.xticks([])
+    plt.yticks([])
+
+plt.show()
+
+cv2.waitKey()
+cv2.destroyAllWindows
