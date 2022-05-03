55e01fe1c7fabfa531c05f00bccb20b97e3ddb14
Laruno <Laman_Art@outlook.com>
2022-05-03 03:22:45 +0100

    Stage 2: Contrast and Noise

--- Preprocessing - Blurring.py
+++ Preprocessing - Blurring.py
@@ -5,12 +5,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+
 Samp1 = cv2.imread("Greyscaled/SampleOne.png")
 Samp2 = cv2.imread("Greyscaled/SampleTwo.png")
 #cv2.imshow("Sample One Original", Samp1)
 #cv2.imshow("Sample Two Original", Samp2)
 gaussianx = GaussianBlur(Samp1, (25,25), 0)
 gaussiany = GaussianBlur(Samp2, (25,25), 0)
+cv2.imwrite("Degraded Images/Sample One Blurred.jpeg", gaussianx)
+cv2.imwrite("Degraded Images/Sample Two Blurred.jpeg", gaussiany)
 
 titles = ["Original 1","Original 2", "Gaussan Output 1","Gaussan Output 2"]
 images = [Samp1,Samp2,gaussianx,gaussiany]
---
+++ Preprocessing - Contrast and Noise.py
@@ -0,0 +1,27 @@
+import cv2
+from cv2 import GaussianBlur
+from matplotlib.pyplot import yticks
+import numpy as np
+import matplotlib.pyplot as plt
+import SaltPepperNoise as noise
+
+Samp1 = cv2.imread("Degraded Images/Sample Two Blurred.jpeg")
+Samp2 = cv2.imread("Greyscaled/SampleThree.png")
+
+Resultx = noise.add_noise(Samp1)
+Resulty = cv2.addWeighted(Samp2,0.5,np.zeros(Samp2.shape, Samp2.dtype), 0, 0)
+
+titles = ["Gaussian Image","Original 3", "Gaussian + Noise Image","Contrast Degradation"]
+
+images = [Samp1,Samp2,Resultx, Resulty]
+for i in range(3):
+    plt.subplot(2,2,i+1)
+    plt.imshow(images[i])
+    plt.title(titles[i])
+    plt.xticks([])
+    plt.yticks([])
+
+plt.show()
+
+cv2.waitKey()
+cv2.destroyAllWindows
